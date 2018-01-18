93d69ab4b192aec6437ded406792d4633a6159ca
Danilo Cândido <danilod07@gmail.com>
2018-01-18 08:30:18 -0300

    Autor.js created with exported classes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
-import $ from 'jquery'; //apelido -> $ from 'jquery'
-import InputCustomizado from './componentes/InputCustomizado';
+import { FormularioAutor, TabelaAutores } from './Autor';
 
 // java -Dspring.datasource.password=root -jar cdcreact-1.0.0-SNAPSHOT.jar 
 
 class App extends Component {
-
-  constructor() {
-    super();
-    this.state = { lista: [], nome: '', email: '', senha: '' };
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  //Componente acabou de ser montado - depois do render
-  componentDidMount(){
-    $.ajax({
-      // http://cdc-react.herokuapp.com/api/autores
-      // http://localhost:8080/api/autores
-      url: "http://cdc-react.herokuapp.com/api/autores",
-      dataType: 'json',
-      success: function(resposta){
-        this.setState({lista: resposta}); //toda vez q o setState é chamado o render vai ser chamado depois
-      }.bind(this) //Aqui usamos a function bind() pra dizer que queremos usar o this do react e não o do jquery
-    });
-  }
 
   //Componente irá de ser montado - antes do render
   //O ideal é usar o willMount com requisição sincrona
@@ -36,47 +13,16 @@ class App extends Component {
     //console.log('chamando - componentWillMount() ');
   }
 
-  enviaForm(evento){
-    evento.preventDefault();
-    
-    $.ajax({
-      url: 'http://cdc-react.herokuapp.com/api/autores',  
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify( {nome: this.state.nome, email: this.state.email, senha: this.state.senha} ),
-      success: function(resposta){
-        this.setState({lista: resposta});
-      }.bind(this),
-      error: function(resposta){
-        console.log('erro');
-      }
-    });
-  }
-
-  setNome(evento){
-    this.setState({nome: evento.target.value});
-  }
-
-  setEmail(evento){
-    this.setState({email: evento.target.value});
-  }
-
-  setSenha(evento){
-    this.setState({senha: evento.target.value});
-  }
-
   render() {
     return (
       <div id="layout">
         <a href="#menu" id="menuLink" className="menu-link">
-            <span></span>
+          <span></span>
         </a>
 
         <div id="menu">
             <div className="pure-menu">
                 <a className="pure-menu-heading" href="#">Company</a>
-
                 <ul className="pure-menu-list">
                     <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
                     <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
@@ -91,42 +37,8 @@ class App extends Component {
           </div>
           
           <div className="content" id="content">
-            <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-
-                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"></InputCustomizado>
-                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"></InputCustomizado>
-                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"></InputCustomizado>
-
-                <div className="pure-control-group">                                  
-                  <label></label> 
-                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                </div>
-              </form>             
-
-            </div>  
-            <div>            
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.map(function(autor) {
-                      return (
-                        <tr key={autor.id}>
-                          <td>{autor.nome}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      );
-                    })
-                  }
-                </tbody>
-              </table> 
-            </div>             
+            <FormularioAutor></FormularioAutor>
+            <TabelaAutores></TabelaAutores>
           </div>
         </div> 
     </div>
